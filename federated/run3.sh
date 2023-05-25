351879ca57a785d51fdf13c6fc1da056191050cc
celinezheng <celine5238@gmail.com>
2023-05-25 15:35:16 +0800

    even radom dis

--- federated/run3.sh
+++ federated/run3.sh
@@ -23,11 +23,11 @@
 # python train.py --dataset fairface --percent 1 --expname uneven --mode CoCoOP --ratio 1.47 --iters 50
 # python train.py --dataset fairface --percent 1 --expname uneven --mode CoCoOP --ratio 1.47 --test
 
-python train.py --dataset fairface --percent 0.5 --expname even --mode ccop --iters 50
-python train.py --dataset fairface --percent 0.5 --expname even --mode ccop --test
-python train.py --dataset fairface --percent 0.5 --expname even --mode CoCoOP --iters 50
-python train.py --dataset fairface --percent 0.5 --expname even --mode CoCoOP --test
-python train.py --dataset fairface --percent 0.5 --expname even --mode fedavg --iters 50
-python train.py --dataset fairface --percent 0.5 --expname even --mode fedavg --test
+python train.py --dataset fairface --percent 0.5 --expname even --mode ccop --iters 50 --gender_dis random_dis --cluster_num 7  --batch 16
+python train.py --dataset fairface --percent 0.5 --expname even --mode ccop --test --gender_dis random_dis --cluster_num 7 
+python train.py --dataset fairface --percent 0.5 --expname even --mode CoCoOP --iters 50 --gender_dis random_dis --batch 16
+python train.py --dataset fairface --percent 0.5 --expname even --mode CoCoOP --test --gender_dis random_dis
+python train.py --dataset fairface --percent 0.5 --expname even --mode fedavg --iters 50 --gender_dis random_dis --batch 16
+python train.py --dataset fairface --percent 0.5 --expname even --mode fedavg --test --gender_dis random_dis
 
 # # # # 4 * (4+2) = 24
