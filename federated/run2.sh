b1bd56e94067c7b040c3856de9bd9b56961db9a3
cehlinezheng <celine5238@gmail.com>
2023-05-25 15:33:54 +0800

    color jotter

--- federated/run2.sh
+++ federated/run2.sh
@@ -1,24 +1,10 @@
 
-# python train.py --dataset fairface --percent 1 --expname uneven --mode ccop --ratio 1.47 --iters 50 --cluster_num 9
-# python train.py --dataset fairface --percent 1 --expname uneven --mode ccop --ratio 1.47 --test --cluster_num 9
 
-# python train.py --dataset fairface --percent 1 --expname uneven --mode ccop --ratio 1.47 --iters 50 --gender_dis single --cluster_num 9
-# python train.py --dataset fairface --percent 1 --expname uneven --mode ccop --ratio 1.47 --iters 50 --gender_dis single --cluster_num 9
-# python train.py --dataset fairface --percent 1 --expname uneven --mode ccop --ratio 1.47 --iters 50 --gender_dis single --cluster_num 7
-# python train.py --dataset fairface --percent 1 --expname uneven --mode ccop --ratio 1.47 --test --gender_dis single --cluster_num 7
+python train.py --dataset fairface --percent 1 --expname uneven --mode ccop --ratio 1.47 --iters 50 --gender_dis random_dis --cluster_num 7 --color_jitter
+python train.py --dataset fairface --percent 1 --expname uneven --mode ccop --ratio 1.47 --test --gender_dis random_dis --cluster_num 7 --color_jitter
 
-# python train.py --dataset fairface --percent 1 --expname uneven --mode ccop --ratio 1.31 --iters 50 --cluster_num 9
-# python train.py --dataset fairface --percent 1 --expname uneven --mode ccop --ratio 1.31 --test --cluster_num 9
-python train.py --dataset fairface --percent 1 --expname uneven --mode ccop --ratio 1.47 --iters 50 --gender_dis random_dis --cluster_num 7
-python train.py --dataset fairface --percent 1 --expname uneven --mode ccop --ratio 1.47 --test --gender_dis random_dis --cluster_num 7
-python train.py --dataset fairface --percent 1 --expname uneven --mode CoCoOP --ratio 1.47 --iters 50 --gender_dis random_dis --cluster_num 7
-python train.py --dataset fairface --percent 1 --expname uneven --mode CoCoOP --ratio 1.47 --test --gender_dis random_dis --cluster_num 7
-
-python train.py --dataset fairface --percent 1 --expname uneven --mode ccop --ratio 1.31 --iters 50 --gender_dis random_dis --cluster_num 7
-python train.py --dataset fairface --percent 1 --expname uneven --mode ccop --ratio 1.31 --test --gender_dis random_dis --cluster_num 7
-python train.py --dataset fairface --percent 1 --expname uneven --mode CoCoOP --ratio 1.31 --iters 50 --gender_dis random_dis --cluster_num 7
-python train.py --dataset fairface --percent 1 --expname uneven --mode CoCoOP --ratio 1.31 --test --gender_dis random_dis --cluster_num 7
-
+python train.py --dataset fairface --percent 1 --expname uneven --mode fedavg --ratio 1.47 --iters 50 --gender_dis random_dis --cluster_num 7
+python train.py --dataset fairface --percent 1 --expname uneven --mode fedavg --ratio 1.47 --test --gender_dis random_dis --cluster_num 7
 
 python train.py --dataset fairface --percent 1 --expname uneven --mode ccop --ratio 1.47 --iters 50 --sam
 python train.py --dataset fairface --percent 1 --expname uneven --mode ccop --ratio 1.47 --test --sam
@@ -27,4 +13,4 @@ python train.py --dataset fairface --percent 1 --expname uneven --mode ccop --ra
 
 
 
-# # # # 4 * (4+2) = 24
+# # # 4 * (4+2) = 24
