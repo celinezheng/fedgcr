b30f35725a9543eb2122cf48b28aa5129f530874
celinezheng <celine5238@gmail.com>
2023-06-18 23:51:40 +0800

    verify perfromance of plain cop

--- federated/112.sh
+++ federated/112.sh
@@ -1,2 +1,14 @@
-python train.py --dataset fairface --percent 0.5 --ratio 1.47 --expname uneven-mix5 --mix5 --cluster 7 --mode ccop --iters 50 --batch 16 --cs --power_cs 5 --netdb
-# python train.py --dataset fairface --percent 0.5 --ratio 1.47 --expname uneven-mix5 --mix5 --cluster 7 --mode ccop --test --batch 16 --cs --power_cs 5 --netdb
+python train.py --dataset fairface --percent 0.5 --ratio 1.47 --expname uneven-mix5 --mix5 --cluster 7 --mode ccop --iters 50 --batch 16 --netdb
+python train.py --dataset fairface --percent 0.5 --ratio 1.47 --expname uneven-mix5 --mix5 --cluster 7 --mode ccop --test --batch 16 --netdb
+
+python train.py --dataset fairface --percent 0.5 --ratio 1.31 --expname uneven-mix5 --mix5 --cluster 7 --mode ccop --iters 50 --batch 16 --netdb
+python train.py --dataset fairface --percent 0.5 --ratio 1.31 --expname uneven-mix5 --mix5 --cluster 7 --mode ccop --test --batch 16 --netdb
+
+python train.py --dataset fairface --percent 0.5 --expname even-mix5 --mix5 --cluster 7 --mode ccop --iters 50 --batch 16 --netdb
+python train.py --dataset fairface --percent 0.5 --expname even-mix5 --mix5 --cluster 7 --mode ccop --test --batch 16 --netdb
+
+python train.py --dataset fairface --ratio 2.15 --split_test --expname uneven --weak_white --mode ccop --cluser 4 --iters 50 --batch 16 
+python train.py --dataset fairface --ratio 2.15 --split_test --expname uneven --weak_white --mode ccop --cluser 4 --iters 50 --test --batch 16
+
+python train.py --dataset fairface --ratio 2.15 --split_test --expname uneven --weak_white --mode ccop --cluser 5 --iters 50 --batch 16 
+python train.py --dataset fairface --ratio 2.15 --split_test --expname uneven --weak_white --mode ccop --cluser 5 --iters 50 --test --batch 16
